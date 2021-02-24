2173115078c1ec7d9c3cbf6fb15435b1b277f2b6
mitchellvdhut <mitchellvdhut@gmail.com>
2021-02-24 21:31:16 +0100

    new markdown parser + syntax highlight

--- components/md-editor.tsx
+++ components/md-editor.tsx
@@ -1,40 +1,88 @@
 import * as React from "react";
 import "react-dom";
 import Editor from "react-markdown-editor-lite";
-import ReactMarkdown from "react-markdown";
 import "react-markdown-editor-lite/lib/index.css";
-
-const gfm = require('remark-gfm')
+import MarkDownIt from 'markdown-it';
+import hljs from 'highlight.js'
+import 'highlight.js/styles/atom-one-dark.css'
 
 export default function MdEditor() {
     const mdEditor = React.useRef(null);
-    const [value, setValue] = React.useState("xxx");
+    const [state, setState] = React.useState({
+        articleTitle: '',
+        articleContent: '',
+    });
 
     const saveArticle = () => {
         if (mdEditor.current) {
             // @ts-ignore
             alert(mdEditor.current.getMdValue());
-            // fetch('http://localhost:3000/wiki-bot/test')
-            //     .then(response => response.json())
-            //     .then(data => console.log(data))
+            fetch('/api/hello', {
+                method: 'POST',
+                body: JSON.stringify({
+                    title: state.articleTitle,
+                    body: state.articleContent
+                }),
+            })
+                .then(response => response.json())
+                .then(data => console.log(data))
 
         }
     };
-
-    const handleEditorChange = ({html, text}: any) => {
-        const newValue = text.replace(/\d/g, "");
-        console.log(newValue);
-        setValue(newValue);
+    const handleChange = (evt) => {
+        const value = evt.target.value;
+        setState({
+            ...state,
+            [evt.target.name]: value
+        });
     };
+    // const handleTitleChange = ({html, text}: any) => {
+    //     const newTitleValue = text.replace(/\d/g, "");
+    //     console.log('1' + newTitleValue);
+    //     setTitleValue(newTitleValue);
+    // };
+    //
+    const handleEditorChange = ({html, text}: any) => {
+        setState({
+            ...state,
+            'articleContent': text
+        });
+    };
+
+    const mdParser = new MarkDownIt({
+        html: true,
+        linkify: true,
+        typographer: true,
+        highlight: function (str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, str).value
+                } catch (__) {}
+            }
+            return '' // use external default escaping
+        },
+
+    });
     return (
         <div className="MdEditor">
-            <button onClick={saveArticle}>Save</button>
-                <Editor
-                ref={mdEditor}
-                value={value}
-                onChange={handleEditorChange}
-                renderHTML={text => <ReactMarkdown plugins={[gfm]} source={text}/>}
+            <div>
+                <input
+                    type={"text"}
+                    name={"articleTitle"}
+                    placeholder={"Pick a title for your article"}
+                    onChange={handleChange}
+                    value={state.articleTitle}
                 />
+                <button onClick={saveArticle}>Save</button>
+            </div>
+            <Editor
+                placeholder={"Start typing your article"}
+                name={"articleContent"}
+                ref={mdEditor}
+                value={state.articleContent}
+                onChange={handleEditorChange}
+                renderHTML={(text) => mdParser.render(text)}
+            />
         </div>
     )
 }
